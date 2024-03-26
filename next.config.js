01afc1964d5f5fde45d173eb905b76c571584df0
Alina <alina.stolbikova.com>
2024-03-26 13:19:11 +0100

    fixed styles.

--- next.config.js
+++ next.config.js
@@ -1,14 +1,18 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+  // (Optional) Export as a static site
+  // See https://nextjs.org/docs/pages/building-your-application/deploying/static-exports#configuration
+  output: "export", // Feel free to modify/remove this option
+
+  // Override the default webpack configuration
   webpack: (config) => {
+    // Ignore node-specific modules when bundling for the browser
+    // See https://webpack.js.org/configuration/resolve/#resolvealias
     config.resolve.alias = {
       ...config.resolve.alias,
       sharp$: false,
       "onnxruntime-node$": false,
     };
-    config.module.rules.push({
-      test: /\.node$/,
-      use: "node-loader",
-    });
     return config;
   },
 };
